6ff3e7a1baacdc9b6bf198e4e630d6d63c7c08d4
MCHCortana <wolokika@gmail.com>
2024-04-17 21:57:44 +0200

    Quotes and Img changing randomly.

---
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -0,0 +1,51 @@
+package cz.czechitas.java2webapps.ukol2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Controller
+public class MainController {
+
+    private static List<String> readAllLines(String resource) throws IOException {
+        //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        //Pomocí metody getResourceAsStream() získáme z classloaderu InpuStream, který čte z příslušného souboru.
+        //Následně InputStream převedeme na BufferedRead, který čte text v kódování UTF-8
+        try (InputStream inputStream = classLoader.getResourceAsStream(resource);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+            //Metoda lines() vrací stream řádků ze souboru. Pomocí kolektoru převedeme Stream<String> na List<String>.
+            return reader
+                    .lines()
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public final Random randomNum = new Random();
+
+    @GetMapping("/")
+    public ModelAndView vypisCitat() throws IOException {
+
+        List<String> citaty = readAllLines("citaty.txt");
+        List<String> imgLinks = readAllLines("imgLinks.txt");
+
+        String citat = citaty.get(randomNum.nextInt(1, 8));
+        String imgLink = imgLinks.get(randomNum.nextInt(1,7));
+
+        ModelAndView result = new ModelAndView("citaty");
+        result.addObject("citat", citat);
+        result.addObject("imgLink", imgLink);
+        return result;
+    }
+}
